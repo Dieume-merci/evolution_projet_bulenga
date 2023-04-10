ab73ab31ffa1098c5dc6a46c0dd076e155be33d2
@CherParent <dieumenshamamba@gmail.comgit config --global user.email dieumenshamamba@gmail.com>
2023-04-10 12:50:53 +0200

    mise creation des menus

--- resources/views/components/sidebar/menu1.blade.php
+++ resources/views/components/sidebar/menu1.blade.php
@@ -1,10 +1,38 @@
 <li class="nav-item">
-    <a class="nav-link collapsed" data-bs-target="#menu2" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-basket2-fill"></i><span>Menu avec sous menu</span><i class="bi bi-chevron-down ms-auto"></i>
+    <a class="nav-link collapsed" data-bs-target="#menu1" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-basket2-fill"></i><span>Gestion de Visites</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
-    <ul id="menu2" class="nav-content collapse" data-bs-parent="#sidebar-nav">
-        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 1</span></a></li>
-        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
-        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
+    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu1_menu1" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>CPN</span></a></li>
+            <ul id="sous_menu1_menu1" class="collapse" data-bs-parent="#sidebar-nav_ok">
+                <li><a href=""><i class="bi bi-circle"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-circle"></i><span>View</span></a></li>
+            </ul>
+        {{-- <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
+        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li> --}}
     </ul>
+    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu2_menu1" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>CPON</span></a></li>
+            <ul id="sous_menu2_menu1" class="collapse" data-bs-parent="#sidebar-nav_ok">
+                <li><a href=""><i class="bi bi-circle"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-circle"></i><span>View</span></a></li>
+            </ul>
+        {{-- <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
+        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li> --}}
+    </ul>
+    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu3_menu1" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>CPS</span></a></li>
+            <ul id="sous_menu3_menu1" class="collapse" data-bs-parent="#sidebar-nav_ok">
+                <li><a href=""><i class="bi bi-circle"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-circle"></i><span>View</span></a></li>
+            </ul>
+        {{-- <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
+        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li> --}}
+    </ul>
+    <a class="nav-link collapsed" data-bs-target="#menu2" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-basket2-fill"></i><span>Gestion Maternité</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
 </li>
--- resources/views/layouts/partials/_sidebar.blade.php
+++ resources/views/layouts/partials/_sidebar.blade.php
@@ -4,13 +4,11 @@
     <li class="nav-item">
       <a class="nav-link" href="">
         <i class="bi bi-speedometer2"></i>
-        <span>Menu 1</span>
+        <span>Dashbord</span>
       </a>
     </li>
     {{-- component pour menu avec sous menu --}}
     <x-sidebar.menu1 />
     {{-- titre regrouper le menu --}}
-    <li class="nav-heading">titre</li>
-    {{-- component pour menus --}}
-    <x-sidebar.menu2 />
+    
 </aside>
