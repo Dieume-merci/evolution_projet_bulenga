7e7cde9b25dfdffcfeeee0c4032a27bb4c632d17
@CherParent <dieumenshamamba@gmail.comgit config --global user.email dieumenshamamba@gmail.com>
2023-04-10 14:47:34 +0200

    adaptation des menus

--- resources/views/components/sidebar/menu1.blade.php
+++ resources/views/components/sidebar/menu1.blade.php
@@ -2,37 +2,86 @@
     <a class="nav-link collapsed" data-bs-target="#menu1" data-bs-toggle="collapse" href="#">
         <i class="bi bi-basket2-fill"></i><span>Gestion de Visites</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
-    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav1">
         <li><a class="nav-link collapsed" data-bs-target="#sous_menu1_menu1" data-bs-toggle="collapse" href="#">
             <i class="bi bi-circle"></i><span>CPN</span></a></li>
-            <ul id="sous_menu1_menu1" class="collapse" data-bs-parent="#sidebar-nav_ok">
+            <ul id="sous_menu1_menu1" class="collapse" data-bs-parent="#sidebar-nav1">
                 <li><a href=""><i class="bi bi-circle"></i><span>add</span></a></li>
                 <li><a href=""><i class="bi bi-circle"></i><span>View</span></a></li>
             </ul>
-        {{-- <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
-        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li> --}}
     </ul>
-    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav2">
         <li><a class="nav-link collapsed" data-bs-target="#sous_menu2_menu1" data-bs-toggle="collapse" href="#">
             <i class="bi bi-circle"></i><span>CPON</span></a></li>
-            <ul id="sous_menu2_menu1" class="collapse" data-bs-parent="#sidebar-nav_ok">
+            <ul id="sous_menu2_menu1" class="collapse" data-bs-parent="#sidebar-nav2">
                 <li><a href=""><i class="bi bi-circle"></i><span>add</span></a></li>
                 <li><a href=""><i class="bi bi-circle"></i><span>View</span></a></li>
             </ul>
-        {{-- <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
-        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li> --}}
     </ul>
-    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+    <ul id="menu1" class="nav-content collapse" data-bs-parent="#sidebar-nav3">
         <li><a class="nav-link collapsed" data-bs-target="#sous_menu3_menu1" data-bs-toggle="collapse" href="#">
             <i class="bi bi-circle"></i><span>CPS</span></a></li>
-            <ul id="sous_menu3_menu1" class="collapse" data-bs-parent="#sidebar-nav_ok">
+            <ul id="sous_menu3_menu1" class="collapse" data-bs-parent="#sidebar-nav3">
                 <li><a href=""><i class="bi bi-circle"></i><span>add</span></a></li>
                 <li><a href=""><i class="bi bi-circle"></i><span>View</span></a></li>
             </ul>
-        {{-- <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
-        <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li> --}}
     </ul>
     <a class="nav-link collapsed" data-bs-target="#menu2" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-basket2-fill"></i><span>Gestion Maternité</span><i class="bi bi-chevron-down ms-auto"></i>
+        <i class="bi bi-basket2-fill"></i><span>Gestion Naissancce</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
+    <ul id="menu2" class="nav-content collapse" data-bs-parent="#sidebar-nav4">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu1_menu2" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>Accouchement</span></a></li>
+            <ul id="sous_menu1_menu2" class="collapse" data-bs-parent="#sidebar-nav4">
+                <li><a href=""><i class="bi bi-circle"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-circle"></i><span>View</span></a></li>
+            </ul>
+    </ul>
+    <ul id="menu2" class="nav-content collapse" data-bs-parent="#sidebar-nav5">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu2_menu2" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>Deces</span></a></li>
+            <ul id="sous_menu2_menu2" class="collapse" data-bs-parent="#sidebar-nav5">
+                <li><a href=""><i class="bi bi-plus"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-eye"></i><span>View</span></a></li>
+            </ul>
+    </ul>
+    <ul id="menu2" class="nav-content collapse" data-bs-parent="#sidebar-nav5">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu3_menu2" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>type Accouchement</span></a></li>
+            <ul id="sous_menu3_menu2" class="collapse" data-bs-parent="#sidebar-nav5">
+                <li><a href=""><i class="bi bi-plus"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-eye"></i><span>View</span></a></li>
+            </ul>
+            {{-- <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
+            <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li> --}}
+    </ul>
+    <a class="nav-link collapsed" data-bs-target="#menu3" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-basket2-fill"></i><span>Gestion VIH</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="menu3" class="nav-content collapse" data-bs-parent="#sidebar-nav4">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu1_menu3" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>Depistage</span></a></li>
+            <ul id="sous_menu1_menu3" class="collapse" data-bs-parent="#sidebar-nav4">
+                <li><a href=""><i class="bi bi-circle"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-circle"></i><span>View</span></a></li>
+            </ul>
+    </ul>
+    <ul id="menu3" class="nav-content collapse" data-bs-parent="#sidebar-nav5">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu2_menu3" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>Methode</span></a></li>
+            <ul id="sous_menu2_menu3" class="collapse" data-bs-parent="#sidebar-nav5">
+                <li><a href=""><i class="bi bi-plus"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-eye"></i><span>View</span></a></li>
+            </ul>
+    </ul>
+    <ul id="menu3" class="nav-content collapse" data-bs-parent="#sidebar-nav5">
+        <li><a class="nav-link collapsed" data-bs-target="#sous_menu3_menu3" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-circle"></i><span>Sous Methode</span></a></li>
+            <ul id="sous_menu3_menu3" class="collapse" data-bs-parent="#sidebar-nav5">
+                <li><a href=""><i class="bi bi-plus"></i><span>add</span></a></li>
+                <li><a href=""><i class="bi bi-eye"></i><span>View</span></a></li>
+            </ul>
+            {{-- <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li>
+            <li><a href=""><i class="bi bi-circle"></i><span>sous menu 2</span></a></li> --}}
+    </ul>
 </li>
